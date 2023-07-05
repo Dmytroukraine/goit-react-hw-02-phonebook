c23ef58a4e0328d05c88080de8a3e94f28c927f1
Dmytro Kozlovskyi <dnfdim@gmail.com>
2023-07-05 20:00:04 +0300

    fix block + normalize

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -2,19 +2,20 @@ import PropTypes from 'prop-types';
 import style from './Filter.module.css';
 
 const Filter = ({ value, onChange }) => (
-  <label className={style.search}>
-    
-    <input
-      type="text"
-      name="filter"
-      placeholder=" "
-      className={style.inputName}
-      title="Enter search name"
-      onChange={onChange}
-      value={value}
-    />
-    <button type="reset"></button>
-  </label>
+  <div className={style.searchContainer}> {/* Додаємо обгортку для центрування */}
+    <label className={style.search}>
+      <input
+        type="text"
+        name="filter"
+        placeholder=" "
+        className={style.inputName}
+        title="Enter search name"
+        onChange={onChange}
+        value={value}
+      />
+      <button type="reset"></button>
+    </label>
+  </div>
 );
 
 Filter.propTypes = {
